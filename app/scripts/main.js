f90db13ca75f83e4aa290b4b32b1c6f395fa0676
Mike Zevitas <mzevitas@gmail.com>
2015-07-17 13:51:11 -0400

    structure updates

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -24,12 +24,12 @@ angular.module('app', [ 'ui.router', 'ngCookies','UserModule', 'ngMaterial'])
 //**************************Main Routes**********************************
 		      .state( 'login', {
 		        url: '/',
-		        templateUrl: 'scripts/users/login.tpl.html',
+		        templateUrl: 'views/login.tpl.html',
 		        controller: 'UserCtrl'
 		      })
 		      .state('register', {
 		        url: '/register',
-		        templateUrl: 'scripts/users/register.tpl.html',
+		        templateUrl: 'views/register.tpl.html',
 		        controller: 'UserCtrl'  
 		      })
 		      .state('portal', {
@@ -40,70 +40,70 @@ angular.module('app', [ 'ui.router', 'ngCookies','UserModule', 'ngMaterial'])
 //**************************Gig Routes**********************************
 		      .state('portal.single', {
 		      	url: '/single/{id}',
-               templateUrl: 'scripts/gigs/single.gig.tpl.html',
+               templateUrl: 'views/single.gig.tpl.html',
                controller: 'SingleCtrl'
 				})
 		      
 		      .state('portal.addgig', {
 		      	url:'/addgig',
-		      	templateUrl: 'scripts/gigs/add.gig.tpl.html'
+		      	templateUrl: 'views/add.gig.tpl.html'
 		      })
 //*************************Set Routes**********************************
 
 		      .state('portal.singleSet', {
 		      	url: '/singleSet/{id}',
-               templateUrl: 'scripts/sets/single.edit.sets.tpl.html',
+               templateUrl: 'views/single.edit.sets.tpl.html',
                controller: 'SingleSetCtrl'
 				})
 
 		      .state('portal.download', {
 		      	url: '/singleSetdownload/{id}',
-               templateUrl: 'scripts/sets/download.pdf.tpl.html',
+               templateUrl: 'views/download.pdf.tpl.html',
                controller: 'SingleSetCtrl'
 				})
 		      
 		      .state('portal.addset', {
 		      	url:'/addset',
-		      	templateUrl: 'scripts/sets/add.sets.tpl.html'
+		      	templateUrl: 'views/add.sets.tpl.html'
 		      })
 //**************************Song Routes**********************************
 		      .state('portal.songList', {
 		      	url:'/songlist',
-		      	templateUrl: 'scripts/songs/songs.tpl.html',
+		      	templateUrl: 'views/songs.tpl.html',
 		      	controller: 'SongCtrl'
 		      })
 		      .state('portal.singleSong', {
 		      	url: '/singleSong/{id}',
-               templateUrl: 'scripts/songs/single.song.tpl.html',
+               templateUrl: 'views/single.song.tpl.html',
                controller: 'EditSongCtrl'
 				})
 		      
 		      .state('portal.addSong', {
 		      	url:'/addsong',
-		      	templateUrl: 'scripts/songs/add.songs.tpl.html',
+		      	templateUrl: 'views/add.songs.tpl.html',
 		      	controller: 'SongCtrl'
 		      })
 //**********************************Venue Routes**********************************
 		      .state('portal.venueList', {
 		      	url:'/venuelist',
-		      	templateUrl: 'scripts/venues/venues.tpl.html',
+		      	templateUrl: 'views/venues.tpl.html',
 		      	controller: 'VenueCtrl'
 		      })
 		      .state('portal.singleVenue', {
 		       url: '/singleVenue/{id}',
-               templateUrl: 'scripts/venues/single.venue.tpl.html',
+               templateUrl: 'views/single.venue.tpl.html',
                controller: 'EditVenueCtrl'
 				})
 
 		      .state('portal.addvenue', {
 		      	url:'/addvenue',
-		      	templateUrl: 'scripts/venues/add.venue.tpl.html',
+		      	templateUrl: 'views/add.venue.tpl.html',
 		      	controller: 'VenueCtrl'
 		      })
 
 		      .state('portal.venueContact', {
 		      	url:'/addContactList',
-		      	templateUrl: 'scripts/venues/venue.contact.tpl.html',
+		      	templateUrl: 'views/venue.contact.tpl.html',
 		      	controller: 'VenueCtrl'
 		      })
 
